0fb322761f9c53cbef9672faa7f55650078c04c3
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-06-15 10:41:00 +0530

    animation in JQuery

---
+++ JQuery/Tutorial/Animate/event.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+
+   $(".animate").click(function (){
+      $(".box").animate({
+         width:"500px",
+         height:"500px",
+         fontSize: "28px"
+      })
+   })
+})
