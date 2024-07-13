c8d38e60f36c3748b0db9131bc73c2ee9c70fa93
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-07-13 20:29:26 +0530

    conditional rendering

--- React JS/03Fragments/src/App.jsx
+++ React JS/03Fragments/src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 
 function App() {
   let items = ["egg", "milk", "bread", "butter"];
+
+  if (items.length === 0) {
+    return <h1>i'm hungry.</h1>;
+  }
   return (
     <>
       <h1>Item List</h1>
@@ -13,6 +17,29 @@ function App() {
     </>
   );
 }
+
+/* Conditional Rendering
+
+1. if-else statements
+  if (items.length === 0) {
+    return <h1>i'm hungry.</h1>;
+  }
+
+2. ternery operator
+3. logical operator
+
+*/
+
+/* Map Method 
+
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+
+*/
 
 /* there are two syntax of fragments 
   1. <React.Fragments></React.Fragments>
