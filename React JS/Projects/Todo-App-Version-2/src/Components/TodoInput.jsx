255330f82cc3fd4dd5829c5d18d0b1b7d6b766a0
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-01 18:08:13 +0530

    Todo App Version 2 base files

---
+++ React JS/Projects/Todo-App-Version-2/src/Components/TodoInput.jsx
@@ -0,0 +1,11 @@
+function TodoInput() {
+  return (
+    <div className="input-todo">
+      <input className="class1" type="text" placeholder="Enter Todo here" />
+      <input className="class2" type="date" />
+      <button className="class3">Add</button>
+    </div>
+  );
+}
+
+export default TodoInput;
---
+++ React JS/Projects/Todo-App-Version-2/src/Components/TodoList.jsx
@@ -0,0 +1,11 @@
+function TodoList() {
+  return (
+    <div className="todo-list">
+      <p className="class1">Buy Milk</p>
+      <p className="class2">29/07/2024</p>
+      <button className="class3">Delete</button>
+    </div>
+  );
+}
+
+export default TodoList;
