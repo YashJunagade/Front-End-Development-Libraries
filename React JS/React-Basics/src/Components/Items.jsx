85651d154a4dbf62cb30f11aebdf1535755996da
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-01 16:23:47 +0530

    css module

--- React JS/React-Basics/src/Components/Items.jsx
+++ React JS/React-Basics/src/Components/Items.jsx
@@ -1,7 +1,10 @@
+import style from "./Items.module.css";
+
 function Items({ item }) {
   return (
     <>
-      <li>{item}</li>
+      <li className={style.list}>{item}</li>
+      {/* <li className={style["list"]}>{item}</li> */}
     </>
   );
 }
