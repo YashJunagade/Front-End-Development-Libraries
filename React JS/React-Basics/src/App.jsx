d963a0c08a0c07cf0ffac6207edc338d16bd5b92
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-02 18:49:44 +0530

    Passing components as container

--- React JS/React-Basics/src/App.jsx
+++ React JS/React-Basics/src/App.jsx
@@ -1,15 +1,16 @@
 import Heading from "./Components/Heading";
 import ItemList from "./Components/ItemList";
 import ErrorMsg from "./Components/ErrorMsg";
+import Container from "./Components/Container";
 
 function App() {
   let foodItems = ["Salad", "Milk", "butter", "seeds", "Ghee"];
   return (
-    <>
+    <Container>
       <Heading></Heading>
       <ErrorMsg foodItems={foodItems}></ErrorMsg>
       <ItemList foodItems={foodItems}></ItemList>
-    </>
+    </Container>
   );
 }
 
