e12ac7494bea95460a217331135ee0bfeaefec0f
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-08-08 12:02:37 +0530

    created basic website structure

--- React JS/Projects/Social-Media/src/App.jsx
+++ React JS/Projects/Social-Media/src/App.jsx
@@ -1,5 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import "./App.css";
+
 function App() {
-  return;
+  return (
+    <div className="app-container">
+      <Sidebar></Sidebar>
+      <div className="content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ React JS/Projects/Social-Media/src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import Post from "./Post";
+
+function PostList() {
+  return (
+    <>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+    </>
+  );
+}
+
+export default PostList;
